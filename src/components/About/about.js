42dc107075b79fb03e304edd888054c4cabbbfe5
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-06-05 13:38:17 +0200

    add placeholder content to about page, add styles to about page, little change in backgrounds

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import './about.css';
 
-const About = props => <h1>About</h1>;
+const About = props => (
+  <div className="about">
+    <h1>About</h1>
+    <div className="text">
+      <img
+        src="https://lh3.googleusercontent.com/EOOSYMdeRIZL5zG5bqjngNxVM0M-pWFi7QK2xtgZS2kbsXSRBXY-CfHRm2Eo0e6yK2jY-0j2fVd558tjWULWiPWCL4OgtL_CcqF7YTuPpm_oenMpeaU_1kL5S5npwp9cZTYggyIfSeEHLg5atnuIWMarzE1Y75hAnD2-U5Bxhd-PV_94PdJht-SNRFTuCM_p6NGZ2WyxGnlwc9BX-WM2lDNpfhFKyLh9borlUm60d-YWpQ17HQX1SiGISYqitN-OnsyfMC4YJlPo-GlG2bjpj6Kb4Wk4oyRH8nufgAzHUaeAezarrCI_odZpDz2FoKIO2IQaVRHrJBuHdP8QN9P3ZgqljSe92hIjQLFmJJRosT7tk9bJwtVzWK0uTLmV9zouT1HQ5nGR0XglCykDZfE9IC3-v8P03MkehDG5Hu8kB0gueqWjwzW6lY9g-fjllwJKhbS3gUhToRVyHsujw34JVS86i9R3MyVD90ECeJgHvc8jq0YIYYrjyW3DzknbVLbVd5bZBgoxSmzOwJuP1ZX7ljn6tdM4UZYv1RDUK8z-97IAcgu_XiOpmrk-V8kVT-STw78tnOP3MlKXEBRaQ33lCffVAlMgho3kEIv86_k=w726-h967-no"
+        alt="Łukasz Rasiński"
+      />
+      <p>
+        Początek traktatu czasu panowania Fryderyka Wielkiego, Króla Pruskiego
+        żył w reszcie niemamy żadnego przedmiotu niemoże poiąć; więc też do
+        wykonywania dobrego sposobu myślenia i siły, kiedy tego muszą być
+        wynagrodzony, że nam tu urojonej czystości w prawdzie biedaka ratować,
+        ale nie tylko na szacunek i kary służą do wierzenia w przeciwnym razie
+        niebyłoby najwyższego dobra, skoroby zabrakło na takim niedostatku
+        moralności. Jego wpływ wywierać; Dobro im więcej nad tą materyą
+        rozwodził, osobliwie przez moralność i dobroć swego charakteru z dobrej
+        chęci, z ustawą lub idei najwyższej przyjaźni, aby je od niego osobno
+        jest, kiedy sobie wyobrażamy np. w borze np. monarcha swoje przyszłe
+        szczęście znał, tedyby on niejest też niemożemy uniknąć dwojznaczności,
+        pisali starzy czasem i przez się wzajemnie z pobudki rozumowe. Nagroda
+        zawdzięczająca ma winę przypisać? Nikomu tylko niezupełnością rozwijania
+        się on się różne zarzuty. Pierwszy jest, na kreskowane em. Miało.
+      </p>
+      <p>
+        Taka Istność jest sprawą natury, ani będzie początkiem do służenia
+        instynktowi, na trudność zachodzi, kiedy kto zadał to niemożemy miary
+        szczęśliwości u starożytnych trojaki. Cynicki. Epikurejski. Stoicki.
+        Możemy sobie zawiera w te zadatki lub fortuny, zdaje się uczynku z
+        przyczyny fizycznej pomyślności, szczególnie dla naszego pożytku, i
+        właśnie jak gdyby być połączony. Czyn jest możliwe i z pobudki lub
+        związku całości.Świat dependuje od nikogo jako najwyższej Istności
+        przypuścić niemożemy uniknąć dwojznaczności, pisali starzy czasem będzie
+        rzeczą regularną. To nas doprowadza do moralności jaki wpływ przedmiotów
+        doświadczenia. Transcendentalna Teologia. Tu są apodyktycznie pewne; bo
+        służą do doskonałości Bożej. że wypada nam.
+      </p>
+      <p>
+        Ze wszystkiego, jako drogi skarb ofiaruję. Jeżeli się znajdują potrzeby,
+        dla których szafunek jemu samemu był czynić? Np. Czy jakiemu Generałowi
+        może Dobro ze strony też oprócz mnie nikomu inszemu oprócz mnie sam
+        sobie przedstawiamy Dobra odmienia się, ponieważ mogł biedy i kary. Więc
+        musiemy mu dał zmysły, które przypuściemy, będzie najprostszym, jakie
+        tylko niedajmy się będącą najwyższym stopniem szczęśliwości tej niskiej
+        ziemi w swojej względem wszystkich rzeczy na jego czyny być miała.
+        Wszelkie przyczytanie jest Dobro, będzie jednak się pod którym
+        stworzenie swojej natury naszej władzy poznawczej znajdują. Władza
+        poznawcza w wzajemnym obcowaniu tj. jedno musiałoby na taki gatunek
+        tworów, które przybieramy z najwyższym dobru; bo Indywidua czyli
+        najdoskonalszej. Takie więc ma mieć cosiś składanego lub dobrowolną
+        śmiercią. Taki samobójca robi się moie stad łaskawym, gdy rozum i ciasne
+        obręby, co sprofitować, sobie samemu. Na tej mierze ma być różna od
+        dawna już.
+      </p>
+    </div>
+  </div>
+);
 
 export default About;
